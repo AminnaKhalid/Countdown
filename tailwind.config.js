2fd350c5a47f739968afd0e9f7bbee698393b5bb
Abdul Rafay <raffyabdul54@gmail.com>
2023-08-26 12:16:40 +0500

    modified the content in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*","/index.html","/day.html","date.html","countdown.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
